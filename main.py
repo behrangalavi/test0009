f058df42c4bbcd599b49be81ec6a50138fd3f4fa
Behrang Alavi <90796287+behrangalavi@users.noreply.github.com>
2022-05-17 09:14:46 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
 
+from flask import Flask
+from flask import render_template
+from flask import request
+
+from flask import Flask, redirect, url_for, request
+app = Flask(__name__)
+
+@app.route('/test', methods=['GET'])
+def test():
+    return 'it works!'
