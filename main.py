d1a7924211e9fa632ed4534d9b3c8023665f12de
Behrang Alavi <behrang@mogenius.com>
2022-05-17 18:10:12 +0200

    testing

--- main.py
+++ main.py
@@ -5,12 +5,10 @@ import os
 
 app = Flask(__name__)
 
-
 @app.route('/')
 def home():
     return 'Random word! %s' % (r.get_random_word())
 
-
 if __name__ == "__main__":
-    port = int(os.environ.get('PORT', 5000))
+    port = int(os.environ.get('PORT', 5001))
     app.run(debug=True, host='0.0.0.0', port=port)
