d337fc4ad523bd1ffe867cb62e8fd80ead58b2da
Behrang Alavi <90796287+behrangalavi@users.noreply.github.com>
2022-05-17 18:01:49 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
-From flask import Flask
+from flask import Flask
+from random_word import RandomWords
+r = RandomWords()
+import os
+
 app = Flask(__name__)
 
+
 @app.route('/')
-def hello_world:
-    return 'Hey, we have Flask in a Docker container!'
+def home():
+    return 'Hey, we have Flask in a Docker container! %s' % (r.get_random_word())
 
 
-if __name == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+if __name__ == "__main__":
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
